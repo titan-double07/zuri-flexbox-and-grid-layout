69d926ba1ee349f6542b57863be4f24678f6a9fd
Chisom <83100384+titanwarlord007@users.noreply.github.com>
2022-06-16 13:43:10 +0100

    Delete about me page directory

--- about me page/README.md
+++
@@ -1,2 +0,0 @@
-# ABOUT ME SITE
-A task givn by zuri training whick i have taken upon myself to build on
--- about me page/index.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector('.hamburger')
-const hamMenu=document.querySelector('.ham-menu')
-const hamburger= ()=>{
-
-    btn.classList.toggle('open')
-hamMenu.classList.toggle('hidden')
-}
-
-btn.addEventListener('click', hamburger)
